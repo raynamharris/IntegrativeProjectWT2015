34ede95cf9dc40608b4b581ea75c6446ab1f866a
raynamharris <rayna.harris@utexas.edu>
2017-04-12 12:24:01 -0500

    new pca plot

--- RmdFiles/figureoptions.R
+++ RmdFiles/figureoptions.R
@@ -3,13 +3,13 @@ colorvalAPA <- c("#404040", "#f4a582", "#ca0020")
 #f4a582 ## pink - same
 #404040 ## darkgrey - yoked
 
-colorvalAPA2 <- c("##bababa", "#404040","##f4a582", "#ca0020")
+colorvalAPA2 <- c("##bababa", "#404040","#f4a582", "#ca0020")
 #ca0020 ## red - conflict 
 #f4a582 ## pink - same
 #bababa ## grey - yoked to same
 #404040 ## darkgrey - yoked to conflict
 
-colorvalPunch <- c("#d95f02","#7570b3", "#1b9e77" )
+colorvalPunch <- c("#d95f02","#7570b3", "#1b9e77")
 
 
 
