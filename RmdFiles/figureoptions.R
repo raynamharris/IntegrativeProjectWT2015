85713a105355aeea3d034f543e10285302ae2b9b
raynamharris <rayna.harris@utexas.edu>
2017-08-02 10:14:59 -0500

    changing colors

--- RmdFiles/figureoptions.R
+++ RmdFiles/figureoptions.R
@@ -1,23 +1,17 @@
-colorvalAPA <- c("#404040", "#f4a582", "#ca0020")
-#ca0020 ## red - conflict 
-#f4a582 ## pink - same
+colorvalAPA <- c("#404040", "#b2182b", "#67001f")
 #404040 ## darkgrey - yoked
-
-colorvalAPA2 <- c("##bababa", "#404040","#f4a582", "#ca0020")
-#ca0020 ## red - conflict 
-#f4a582 ## pink - same
-#bababa ## grey - yoked to same
-#404040 ## darkgrey - yoked to conflict
+#b2182b ## red - consistent
+#67001f ## maroon - conflict 
 
 colorvalPunch <- c("#d95f02","#1b9e77", "#7570b3")
-
-
 
 ## pheatmap
 colorpalette <-  colorRampPalette(c("Deep Sky Blue 3", "white", "red"))( 30 )
 
 ann_colors1 = list(
-  Group =  c(control = (values=c("#404040")), consistent = (values=c("#f4a582")), conflict = (values=c("#ca0020"))),
+  Group =  c(control = (values=c("#404040")), 
+             consistent = (values=c("#b2182b")), 
+             conflict = (values=c("#67001f"))),
   Region = c(CA1 = (values=c("#7570b3")),
             CA3 = (values=c("#1b9e77")), 
             DG = (values=c("#d95f02"))))
@@ -25,8 +19,8 @@ ann_colors1 = list(
 
 session_colors1 = list(
   APA =  c(Control = (values=c("#404040")), 
-           Consistent = (values=c("#f4a582")),
-           Conflict = (values=c("#ca0020"))),
+           Consistent = (values=c("#b2182b")),
+           Conflict = (values=c("#67001f"))),
   Session =  c(Hab = (values=c("#eff3ff")), 
                T1 = (values=c("#bdd7e7")),
                T2 = (values=c("#6baed6")),
