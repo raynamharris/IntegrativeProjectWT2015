0858d2160e52c8fdde3a5460c7f181470bbb4ca8
raynamharris <rayna.harris@utexas.edu>
2019-03-08 21:29:03 -0600

    remove obsolete color palettes

--- scripts/figureoptions.R
+++ scripts/figureoptions.R
@@ -1,32 +1,14 @@
-# Behavior colors 
+# this file stores color palettes
 
-# for mean and sd plots
+# for mean and sd plots of behavior 
 ann_colors_APA2 = list(
   APA2 =  c('yoked-consistent' = (values=c("#404040")), 
             'consistent' = (values=c("#ca0020")),
             'yoked-conflict' = (values=c("#bababa")),
             'conflict' = (values=c("#f4a582"))))
 
-# for beahvir heatmap
-APA2session = list(
-   APA2 =  c('yoked-consistent' = (values=c("#404040")), 
-            'consistent' = (values=c("#ca0020")),
-            'yoked-conflict' = (values=c("#bababa")),
-            'conflict' = (values=c("#f4a582"))),
-   Session =  c(Hab = (values=c("#ffffff")), 
-               T1 = (values=c("#f0f0f0")),
-               T2 = (values=c("#d9d9d9")),
-               T3 = (values=c("#bdbdbd")),
-               Retest = (values=c("#969696")),
-               T4_C1 = (values=c("#737373")),
-               T5_C2 = (values=c("#525252")),
-               T6_C3 = (values=c("#252525")),
-               Retention = (values=c("#000000"))))
 
-colorvalPunch <- c("#d95f02","#1b9e77", "#7570b3")
-## DG  "#d95f02"
-## CA3 "#1b9e77"
-## CA1 "#7570b3")
+# for beahvior and physiology
 
 colorvalAPA00 <-  c( "#404040","#ca0020", "#bababa", "#f4a582")
 #404040 ## darkgrey - Yoked_NoConflict
@@ -34,62 +16,27 @@ colorvalAPA00 <-  c( "#404040","#ca0020", "#bababa", "#f4a582")
 #bababa ## light grey - Yoked_Conflict
 #f4a582 ## pink - Trained_Conflict
 
-colorvalAPA4<-  c( "#bababa","#f4a582")
-#bababa ## light grey - yoked-conflict
-#f4a582 ## pink - conflict
-
-colorvalAPA5<-  c( "#404040","#ca0020")
-#404040 ## darkgrey - yoked-consistent
-#ca0020 ## red - consistent 
-
-colorvalAPA6<-  c( "#f4a582","#ca0020")
-#f4a582 ## pink - conflict
-#ca0020 ## red - consistent 
-
-colorvalAPA7<-  c( "#bababa","#404040")
-#bababa ## light grey - yoked-conflict
-#404040 ## darkgrey - yoked-consistent
 
 
-DGConflictControl <- c("#bababa","#f4a582")
-DGConsistentControl <- c("#bababa","#ca0020")
-DGConflictConsistent <- c("#f4a582","#ca0020")
 
+# for all data included RNA-seq plots
 
-volcano1 <-  c("consistent" = "#ca0020",
-           "yoked_consistent" = "#404040", 
-           "neither" = "#d9d9d9")
+colorvalPunch <- c("#d95f02","#1b9e77", "#7570b3")
+## DG  "#d95f02"
+## CA3 "#1b9e77"
+## CA1 "#7570b3")
 
-volcano2 <-  c("consistent" = "#ca0020",
-               "conflict" = "#f4a582",
-               "neither" = "#d9d9d9")
-
-volcano3 <-  c("yoked_consistent" = "#404040",
-               "yoked_conflict" = "#bababa", 
-               "neither" = "#d9d9d9")
-
-volcano4 <-  c("conflict" = "#f4a582",
-               "yoked_conflict" = "#bababa", 
-               "neither" = "#d9d9d9")
-
-volcano5 <-  c("yes" = "red",
-               "no" = "black", 
-               "none" = "#d9d9d9")
-               
 volcanoDGvCA3 <- c("CA3" = "#1b9e77",
                    "DG" = "#d95f02", 
-                    "neither" = "#d9d9d9")  
-                    
+                   "neither" = "#d9d9d9")  
+
 volcanoDGvCA1 <-  c("CA1" = "#7570b3",
                     "DG" = "#d95f02", 
-                     "neither" = "#d9d9d9")            
+                    "neither" = "#d9d9d9")            
 
 volcanoCA3vCA1 <-  c("CA1" = "#7570b3",
                      "CA3" = "#1b9e77", 
                      "neither" = "#d9d9d9")
-
-
-colorvalavoidance <- c("black","red")
 
 
 ann_colors4 = list(
@@ -101,34 +48,39 @@ ann_colors4 = list(
             'conflict' = (values=c("#f4a582")),
             'consistent' = (values=c("#ca0020"))))
 
-ann_colors5 = list(
-  APA2 =  c('yoked_consistent' = (values=c("#404040")), 
-            'consistent' = (values=c("#ca0020"))),
-  Punch = c(CA1 = (values=c("#7570b3")),
-            CA3 = (values=c("#1b9e77")), 
-            DG = (values=c("#d95f02"))))
 
-ann_colors6 = list(
-  APA2 =  c('conflict' = (values=c("#f4a582")), 
-            'consistent' = (values=c("#ca0020"))),
-  Punch = c(CA1 = (values=c("#7570b3")),
-            CA3 = (values=c("#1b9e77")), 
-            DG = (values=c("#d95f02"))))
 
-ann_colors7 = list(
-  APA2 =  c('yoked_consistent' = (values=c("#404040")), 
-            'yoked_conflict' = (values=c("#bababa"))),
-  Punch = c(CA1 = (values=c("#7570b3")),
-            CA3 = (values=c("#1b9e77")), 
-            DG = (values=c("#d95f02"))))
+# for subfield volcano plots
+
+volcano1 <-  c("consistent" = "#ca0020",
+           "yoked_consistent" = "#404040", 
+           "neither" = "#d9d9d9")
+
+volcano3 <-  c("yoked_consistent" = "#404040",
+               "yoked_conflict" = "#bababa", 
+               "neither" = "#d9d9d9")
+
+# for avoidance volcano plots and heatmaps
+
+volcano5 <-  c("yes" = "red",
+               "no" = "black", 
+               "none" = "#d9d9d9")
+               
+colorvalavoidance <- c("black","red")
 
 ann_colors8 = list(
   avoidance =  c('yes' = (values=c("red")), 
-            'no' = (values=c("black"))),
+                 'no' = (values=c("black"))),
   Punch = c(CA1 = (values=c("#7570b3")),
             CA3 = (values=c("#1b9e77")), 
             DG = (values=c("#d95f02"))))
 
+
+# for integrative correlation anaylsis
+
+colorvalAPA6<-  c( "#f4a582","#ca0020")
+#f4a582 ## pink - conflict
+#ca0020 ## red - consistent 
 
 ann_colorsLevel = list(
   Level = c(
