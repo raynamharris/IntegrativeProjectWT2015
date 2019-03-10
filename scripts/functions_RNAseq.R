91b66e62aee504491ac68bfd5fb0680c1dd81785
raynamharris <rayna.harris@utexas.edu>
2019-03-10 00:50:00 -0600

    move function back

--- scripts/functions_RNAseq.R
+++ scripts/functions_RNAseq.R
@@ -1,3 +1,16 @@
+resvals <- function(contrastvector, mypval){
+  res <- results(dds, contrast = c(contrastvector[1],contrastvector[2],contrastvector[3]), independentFiltering = T)
+  sumpvalue <- sum(res$pvalue < mypval, na.rm = TRUE)
+  #print(sumpvalue)
+  sumpadj <- sum(res$padj < mypval, na.rm = TRUE)
+  print(sumpadj)
+  vals <- cbind(res$pvalue, res$padj)
+  pvalcolname <- as.character(paste("pval",contrastvector[1],contrastvector[2],contrastvector[3], sep=""))
+  padjcolname <- as.character(paste("padj",contrastvector[1],contrastvector[2],contrastvector[3], sep=""))
+  colnames(vals) <- c(pvalcolname, padjcolname)
+  return(vals)
+}
+
 myhistogram <- function(contrastvector, mypval){
   res <- results(dds, contrast = c(contrastvector[1],contrastvector[2],contrastvector[3]), independentFiltering = T)
   vals <- cbind(res$pvalue)
