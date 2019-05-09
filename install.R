b3ec9b0253ee3eb778fe94135caf23f9ae0676ad
raynamharris <rayna.harris@utexas.edu>
2019-05-09 16:40:40 -0700

    now with multiple core

--- install.R
+++ install.R
@@ -1,39 +1,12 @@
-install.packages("tidyverse")
-install.packages("knitr")
-install.packages("rmarkdown")
+pkgs = c("tidyverse", "knitr", "rmarkdown",
+         "plyr", "reshape2", "cowplot", "factoextra", "car",  
+         "pheatmap", "viridis", "magrittr", "genefilter",  
+         "ggrepel","colorblindr", "xtable", "Hmisc", "corrplot",
+         "magick", "ggrepel", "stringr", "kableExtra",
+         "devtools", "UpSetR","BiocManager")
+ncores = parallel::detectCores()
+install.packages(pkgs, Ncpus = ncores)
 
-install.packages("plyr")  
-install.packages("reshape2")  
-install.packages("cowplot")  
-install.packages("factoextra")   
-install.packages("car")  
-install.packages("pheatmap")   
-install.packages("viridis")  
-
-install.packages("magrittr") 
-install.packages("genefilter")  
-
-install.packages("ggrepel")  
-install.packages("colorblindr")  
-
-install.packages("xtable")
-
-install.packages("Hmisc")
-install.packages("corrplot")
-
-install.packages("magick")
-
-install.packages("ggrepel")
-install.packages("stringr")
-install.packages("kableExtra")
-
-install.packages("BiocManager") 
 BiocManager::install("DESeq2")
 BiocManager::install("edgeR")
-
-install.packages("devtools")
 devtools::install_github("clauswilke/ggtextures")
-
-install.packages("UpSetR")
-
-
